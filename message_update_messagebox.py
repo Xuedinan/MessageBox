46f55240ebad7033a9c3034fb2afc9e5a9f6c4bc
Xuedinan Gao <xuedinangao@Xuedinans-MacBook-Pro.local>
2023-11-17 23:42:04 -0800

    update msg file

--- message_update_messagebox.py
+++ message_update_messagebox.py
@@ -19,20 +19,21 @@ import login_logout_messagebox as login
 command_error = "Command is wrong, please enter correct command key word"
 message_command = "Do you want to write new message or review historical message?\n\n Enter: write (to write new message)\n Enter: review (to review historical message)\n"
 select_user = "Please enter user name to write message to: "
+username_erro = "Username not exsit, please enter correct username"
 username_list = "Below users are in the system right now: \n"
 write_command = "write"
 write_command_msg = "Please enter your message: "
 review_command = "review"
 
 
-def read_message_file(file="user_message.txr"):
+def read_message_file(file="user_message.txt"):
     pass
 
 
-def write_message_file(file="user_message.txr"):
+def write_message_file(file="user_message.txt"):
     try:
         with open(file, "a+") as file:
-            pass
+            file.write(str(select_username_message(user_name_list()))+"\n")
     except EOFError as erro:
         print("Open message file is wrong ", erro)
 
@@ -43,10 +44,26 @@ def user_name_list():
     return name_list
 
 
+def select_username_message(name_list):
+    user_message = {}
+    user_name = input(select_user)
+    while user_name not in name_list:
+        print(username_erro)
+        print("Existing users are ", *user_name_list())
+        user_name = input(select_user)
+    else:
+        message_to_user = input(write_command_msg)
+        user_message[user_name] = message_to_user
+        return user_message
+
+
 def write_message():
     print(username_list)
-    print(*user_name_list())
-    user_msg = input(select_user)
+    print(*user_name_list(), "\n")
+    write_message_file()
+
+
+write_message()
 
 
 def review_message():
